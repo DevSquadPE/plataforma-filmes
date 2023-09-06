67bd311e11d55abf378837651d7ab5cd3cbeecf0
Suzane Feitosa <suzanefeitosa08@icloud.com>
2023-09-06 16:48:31 -0300

    ajustes css e add js

--- script-feature.js
+++ script-feature.js
@@ -1,7 +1,30 @@
-function StarsFromAPI() {
-    var rating = RatingFromAPI();
-    $(".icon-star").removeClass("active"); 
-    $(".icon-star[data-rating='" + rating + "']").prevAll().addBack().addClass("active");
-  }
-  
-  
+// function StarsFromAPI() {
+//     var rating = RatingFromAPI();
+//     $(".icon-star").removeClass("active"); 
+//     $(".icon-star[data-rating='" + rating + "']").prevAll().addBack().addClass("active");
+//   }
+
+const API_KEY = "60231c33f3cd21e8fdceb8cf48a2e7fb";
+const IMAGE_URL = "https://image.tmdb.org/t/p/original";
+const URL = "https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=pt&sort_by=popularity.desc&include_adult=false"
+
+
+// hover setas - cards populares:
+
+let cards = document.querySelectorAll('.container');
+let buttons = document.querySelectorAll('.buttonPrev');
+
+cards.forEach((card) => {
+  card.addEventListener('mouseover', () => {
+    buttons.forEach((button) => {
+      button.style.opacity = '1';
+    });
+  });
+
+  card.addEventListener('mouseout', () => {
+    buttons.forEach((button) => {
+      button.style.opacity = '0';
+    });
+  });
+});
+
