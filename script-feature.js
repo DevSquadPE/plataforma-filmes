58ac00ed9017568f5a26a9bd3acb6ac40a3b7170
Suzane Feitosa <suzanefeitosa08@icloud.com>
2023-09-11 15:54:29 -0300

    ajustes js e css

--- script-feature.js
+++ script-feature.js
@@ -1,8 +1,3 @@
-// function StarsFromAPI() {
-//     var rating = RatingFromAPI();
-//     $(".icon-star").removeClass("active"); 
-//     $(".icon-star[data-rating='" + rating + "']").prevAll().addBack().addClass("active");
-//   }
 
 // consumo API: 
 
