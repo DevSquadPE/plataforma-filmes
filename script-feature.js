bf77d7b8f04730c2d89c314f88df2fc5885b9838
Thaís Amancio <thaisamancio1300@gmail.com>
2023-09-26 17:39:30 -0300

    Atualização dos dados da API ao card-Slider

--- script-feature.js
+++ script-feature.js
@@ -4,7 +4,11 @@
 const API_KEY = "60231c33f3cd21e8fdceb8cf48a2e7fb";
 const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
-fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false`)
+
+
+
+try {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false`)
 .then(res => {
   if (!res.ok) {
     throw new Error('Erro na solicitação da API');
@@ -13,11 +17,53 @@ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=p
 })
 
 .then((data) => {
-  const filmes = data.results;
-  renderizarListaDeFilmes(filmes);
-})
+  data.results.forEach(filme => {
+    var cardWrapper = document.createElement("div");
+cardWrapper.className = "card-wrapper";
+var slider = document.querySelector(".card-slider");
 
-.catch((error) => console.error(error))
+var cardArticle = document.createElement("article");
+cardArticle.className = "card";
+var img = document.createElement("img");
+img.className = "img-poster";
+img.src = `https://image.tmdb.org/t/p/original/${filme.poster_path}`
+
+var content = document.createElement("div");
+content.className = "card-content";
+
+var category = document.createElement("div")
+category.className = "card-category";
+
+var star = "★";
+
+var titleCategory = document.createElement("p");
+titleCategory.className = ("title-category"); 
+titleCategory.textContent = filme.vote_average + star;
+
+var titleCard = document.createElement("h3");
+titleCard.className = "card-title";
+titleCard.textContent = filme.title; 
+
+cardWrapper.appendChild(cardArticle);
+slider.appendChild(cardWrapper);
+content.appendChild(category);
+content.appendChild(titleCard);
+category.appendChild(titleCategory);
+category.appendChild(titleCard);
+cardArticle.appendChild(img);
+cardArticle.appendChild(content);
+
+  })
+
+}) 
+
+} catch (error) {
+  console.error(error);
+}
+
+//Cards - criação de divs
+
+
 
 // hover setas - cards populares:
 
